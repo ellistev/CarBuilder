b105c5c3847b5d79e5db98f622ad76e9b8d2bca6
ellistev <steven.peter.elliott@gmail.com>
2015-11-20 08:31:18 -0700

    sorting

--- CarBuilder/Program.cs
+++ CarBuilder/Program.cs
@@ -8,78 +8,50 @@ using System.IO;
 
 namespace CarBuilder
 {
-    public static class Program
+    public class Program
     {
+        public static bool added = false;
+        
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            StreamReader input;
-            List<String> objects = new List<string>();
-            ITopologicSortable dependencies = new Dictionary<string, string>();
-
+           
             if (args != null && args.Length == 1)
             {
                 var dependenciesPath = args[0];
                 if (File.Exists(dependenciesPath))
                 {
-                    input = File.OpenText(dependenciesPath);
-
-                    for (string line; (line = input.ReadLine()) != null; )
-                    {
-
-                        var foo = new Foo();
-                        var bar = new Foo();
-                        var parent = new Foo();
-                        bar.DependsOn.Add(foo); // bar depends on foo; it will be sorted to come after foo
-                        parent.Children.Add(bar);
-                        parent.Children.Add(foo);
-                        parent.SortSelfAndDescendents(); // parent.Children now contains the sorted list
-
-                        string[] splitDependancy = Regex.Split(line,"->");
-
-                        dependencies.Add(splitDependancy[0].Trim(), splitDependancy[1].Trim());
-                        Console.WriteLine(line);
-                    }
+                    BuildCar(dependenciesPath);
                 }
 
-                var result = TSort<List<String>>(dependencies, dependencies, false); 
-
             }
             else
             {
-                Console.Write("Usage: CarBuilder [objectfile] [dependenciesfile]");
+                Console.Write("Usage: CarBuilder [dependenciesfilePath]");
             }
-
-
         }
 
-        public static IEnumerable<T> TSort<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> dependencies, bool throwOnCycle = false)
+        private static void BuildCar(string dependenciesPath)
         {
-            var sorted = new List<T>();
-            var visited = new HashSet<T>();
-
-            foreach (var item in source)
-                Visit(item, visited, sorted, dependencies, throwOnCycle);
-
-            return sorted;
-        }
-
-        private static void Visit<T>(T item, HashSet<T> visited, List<T> sorted, Func<T, IEnumerable<T>> dependencies, bool throwOnCycle)
-        {
-            if (!visited.Contains(item))
+            StreamReader input;
+            input = File.OpenText(dependenciesPath);
+            TopologicalSorter<String> sorter = new TopologicalSorter<string>(); 
+            
+            for (string line; (line = input.ReadLine()) != null; )
             {
-                visited.Add(item);
-
-                foreach (var dep in dependencies(item))
-                    Visit(dep, visited, sorted, dependencies, throwOnCycle);
-
-                sorted.Add(item);
+                string[] splitDependancy = Regex.Split(line, "->");
+                var objectName = splitDependancy[0].Trim();
+                var dependantObject = splitDependancy[1].Trim();
+                sorter.AddObjects(objectName);
+                sorter.AddObjects(dependantObject);
+                sorter.SetDependencies(objectName, dependantObject);
+                Console.WriteLine(line);
             }
-            else
-            {
-                if (throwOnCycle && !sorted.Contains(item))
-                    throw new Exception("Cyclic dependency found");
-            }
+
+            var result =  sorter.Sort();
+
+            Console.Write(result);
+
         }
     }
 }
--- CarBuilder/TopologicalSorter.cs
+++ CarBuilder/TopologicalSorter.cs
@@ -1,209 +1,184 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-
+using System.Diagnostics;
 namespace CarBuilder
 {
-    class TopologicalSorter
-    {
-    }
-
-    /// <summary>
-    /// Interface to define objects which can be topologically sorted.
-    /// </summary>
-    /// <typeparam name="T">Type of underlying object.</typeparam>
-    public interface ITopologicSortable<T>
-    {
-        /// <summary>
-        /// Unique identifer of the item.
-        /// </summary>
-        Guid Identifier { get; }
-
-        /// <summary>
-        /// Gets or sets a list of object the current item depends on.
-        /// </summary>
-        ObservableCollection<T> DependsOn { get; set; }
-    }
-
-    /// <summary>
-    /// Extension method to implement topologic sorting. Will return a list where items with dependencies
-    /// will come after items without dependencies.
-    /// </summary>
-    /// <remarks>
-    /// From http://stackoverflow.com/questions/21189222/topological-sort-with-support-for-cyclic-dependencies
-    /// </remarks>
-    public static class TopologicSortExtensions
-    {
-        /// <summary>
-        /// Sorts a list of items with dependencies such that items without dependencies come first.
-        /// </summary>
-        /// <typeparam name="T">Type of list, which must implement <see cref="ITopologicSortable"/>.</typeparam>
-        /// <param name="graph">List of items to sort.</param>
-        /// <returns>A sorted list of item, where items with dependencies after items without dependencies.</returns>
-        public static List<T> TopologicSort<T>(this List<T> graph)
-            where T : ITopologicSortable<T>
+    public class TopologicalSorter<T>
         {
-            int[] state = new int[graph.Count];
-            List<T> list = new List<T>();
+            //**************************************************
+            //
+            // Private members
+            //
+            //**************************************************
 
-            for (int i = 0; i < graph.Count; i++)
+            #region Private members
+
+            /// <summary>
+            /// Gets the dependency matrix used by this instance.
+            /// </summary>
+            private readonly Dictionary<T, Dictionary<T, object>> _matrix = new Dictionary<T, Dictionary<T, object>>();
+
+            #endregion
+
+
+            //**************************************************
+            //
+            // Public methods
+            //
+            //**************************************************
+
+            #region Public methods
+
+            /// <summary>
+            /// Adds a list of objects that will be sorted.
+            /// </summary>
+            public void AddObjects(params T[] objects)
             {
-                state[i] = 1; /* alive */
-            }
-            for (int i = 0; i < graph.Count; i++)
-            {
-                visit(graph, i, list, state);
-            }
-            return list;
-        }
+                // --- Begin parameters checking code -----------------------------
+                Debug.Assert(objects != null);
+                Debug.Assert(objects.Length > 0);
+                // --- End parameters checking code -------------------------------
 
-        private static void visit<T>(List<T> graph, int index, List<T> list, int[] state)
-            where T : ITopologicSortable<T>
-        {
-            if (state[index] == -1) /* dead */
-            {
-                return; // We've done this one already.
-            }
-            if (state[index] == 0) /* processing */
-            {
-                return; // We have a cycle; if you have special cycle handling code do it here.
-            }
-
-            // It's alive. Mark it as undead.
-            state[index] = 0; /* processing */
-
-            foreach (T neighbour in getNeighbours(graph, graph[index]))
-            {
-                visit(graph, graph.IndexOf(neighbour), list, state);
-            }
-
-            state[index] = -1; /* dead */
-
-            list.Add(graph[index]);
-        }
-
-        private static List<T> getNeighbours<T>(List<T> graph, T node)
-            where T : ITopologicSortable<T>
-        {
-            List<T> neighbours = new List<T>();
-            graph.ForEach(x => x.DependsOn.Where(y => y.Identifier == node.Identifier).ToList().ForEach(z =>
-            {
-                neighbours.Add(z);
-            }));
-
-            node.DependsOn.ToList().ForEach(x => neighbours.Add(x));
-
-            List<T> unique = new List<T>();
-            neighbours.ForEach(x =>
-            {
-                if (!unique.Any(y => y.Identifier == x.Identifier))
+                // add to matrix
+                foreach (T obj in objects)
                 {
-                    unique.Add(x);
+                    // add to dictionary
+                    if (_matrix.ContainsKey(obj))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        _matrix.Add(obj, new Dictionary<T, object>());    
+                    }
+                    
                 }
-            });
-
-            // can't be neighbors with yourself
-            if (unique.IndexOf(node) > -1)
-            {
-                unique.Remove(node);
             }
 
-            return unique;
+            /// <summary>
+            /// Sets dependencies of given object.
+            /// This means <paramref name="obj"/> depends on these <paramref name="dependsOnObjects"/> to run.
+            /// Please make sure objects given in the <paramref name="obj"/> and <paramref name="dependsOnObjects"/> are added first.
+            /// </summary>
+            public void SetDependencies(T obj, params T[] dependsOnObjects)
+            {
+                // --- Begin parameters checking code -----------------------------
+                Debug.Assert(dependsOnObjects != null);
+                // --- End parameters checking code -------------------------------
+
+                // set dependencies
+                Dictionary<T, object> dependencies = _matrix[obj];
+                //dependencies.Clear();
+
+                // for each depended objects, add to dependencies
+                foreach (T dependsOnObject in dependsOnObjects)
+                {
+                    dependencies.Add(dependsOnObject, null);
+                }
+            }
+
+            /// <summary>
+            /// Sorts objects based on this dependencies.
+            /// Note: because of the nature of algorithm and memory usage efficiency, this method can be used only one time.
+            /// </summary>
+            public T[] Sort()
+            {
+                // prepare result
+                List<T> result = new List<T>(_matrix.Count);
+
+                // while there are still object to get
+                while (_matrix.Count > 0)
+                {
+                    // get an independent object
+                    T independentObject;
+                    if (!this.GetIndependentObject(out independentObject))
+                    {
+                        // circular dependency found
+                        throw new CircularReferenceException();
+                    }
+
+                    // add to result
+                    result.Add(independentObject);
+
+                    // delete processed object
+                    this.DeleteObject(independentObject);
+                }
+
+                // return result
+                return result.ToArray();
+            }
+
+            #endregion
+
+
+            //**************************************************
+            //
+            // Private methods
+            //
+            //**************************************************
+
+            #region Private methods
+
+            /// <summary>
+            /// Returns independent object or returns NULL if no independent object is found.
+            /// </summary>
+            private bool GetIndependentObject(out T result)
+            {
+                // for each object
+                foreach (KeyValuePair<T, Dictionary<T, object>> pair in _matrix)
+                {
+                    // if the object contains any dependency
+                    if (pair.Value.Count > 0)
+                    {
+                        // has dependency, skip it
+                        continue;
+                    }
+
+                    // found
+                    result = pair.Key;
+                    return true;
+                }
+
+                // not found
+                result = default(T);
+                return false;
+            }
+
+            /// <summary>
+            /// Deletes given object from the matrix.
+            /// </summary>
+            private void DeleteObject(T obj)
+            {
+                // delete object from matrix
+                //if (obj == null)
+                //{
+                //    return;
+                //}
+                _matrix.Remove(obj);
+
+                // for each object, remove the dependency reference
+                foreach (KeyValuePair<T, Dictionary<T, object>> pair in _matrix)
+                {
+                    // if current object depends on deleting object
+                    pair.Value.Remove(obj);
+                }
+            }
+
+
+            #endregion
+        }
+
+        /// <summary>
+        /// Represents a circular reference exception when sorting dependency objects.
+        /// </summary>
+        public class CircularReferenceException : Exception
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="CircularReferenceException"/> class.
+            /// </summary>
+            public CircularReferenceException()
+                : base("Circular reference found.")
+            {            
+            }
         }
     }
-
-
-    public class Foo : ITopologicSortable<Foo>
-    {
-        private bool _dependenciesAreResolved = false;
-        private Guid _identifier;
-
-        /// <summary>
-        /// Child items that will be executed.
-        /// </summary>
-        public ObservableCollection<Foo> Children { get; set; }
-
-        /// <summary>
-        /// List of all pre-requisite Foos that must be executed first.
-        /// </summary>
-        public ObservableCollection<Foo> DependsOn { get; set; }
-
-        /// <summary>
-        /// Gets the unique identifier of the Foo.
-        /// </summary>
-        public Guid Identifier
-        {
-            get
-            {
-                return _identifier;
-            }
-        }
-
-        /// <summary>
-        /// Creates a new Foo
-        /// </summary>
-        public Foo()
-        {
-            _identifier = Guid.NewGuid();
-
-            DependsOn = new ObservableCollection<Foo>();
-            Children = new ObservableCollection<Foo>();
-
-            // Flag that dependencies must be recalculated when collections change
-            DependsOn.CollectionChanged += (s, e) =>
-            {
-                _dependenciesAreResolved = false;
-            };
-
-            Children.CollectionChanged += (s, e) =>
-            {
-                _dependenciesAreResolved = false;
-            };
-        }
-
-        /// <summary>
-        /// Sorts all Foos and child Foos so that the items can be displayed in the correct order in the user interface.
-        /// </summary>
-        public void SortSelfAndDescendents()
-        {
-            if (_dependenciesAreResolved == false)
-            {
-                ResolveDependencies();
-            }
-
-            if (Children.Count > 0)
-            {
-                Children.ToList().ForEach(x => x.SortSelfAndDescendents());
-            }
-        }
-
-        /// <summary>
-        /// Helper function to sort the children of the current item if they are not already sorted.
-        /// </summary>
-        private void ResolveDependencies()
-        {
-            _dependenciesAreResolved = true;
-
-            if (Children.Count == 0)
-            {
-                return;
-            }
-
-            List<Foo> temp = new List<Foo>();
-
-            Children.ToList().TopologicSort().ForEach(x => temp.Add(x));
-
-            Children.Clear();
-
-            temp.ForEach(x => Children.Add(x));
-
-            // notify Children OnPropertyChanged here
-        }
-    }
-}
